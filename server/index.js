6f7d19e347f9b9b67a53ab100b67d3ae78d4f541
Hugo <hugo_lui@rogers.com>
2023-08-19 08:58:38 -0400

    Merge branch 'main' of https://github.com/hugo-lui/hackthesix

--- server/index.js
+++ server/index.js
@@ -7,9 +7,13 @@ app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
 })
 
-async function getEvents(req, res, next) {
+app.get("/:keyword", (req, res) => {
+    res.status(200).json({"urls": res.urls});
+})
+
+app.param("keyword", async (req, res, next, keyword) => {
     try {
-        const endpoint = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=taylorswift&source=ticketmaster&countryCode=CA&apikey=A9TpV8e3qhYwNpALFJPgvNrgQVsQkpVS";
+        const endpoint = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=taylorswift&source=ticketmaster&countryCode=CA&apikey=${process.env.API_KEY}`;
         const response = await fetch(endpoint);
         if(response.ok) {
             const jsonResponse = await response.json();
@@ -18,12 +22,11 @@ async function getEvents(req, res, next) {
             data.forEach((object) => {
                 urls.push(JSON.stringify(object.url));
             });
-            console.log(urls);
-            //res.status(200).json({"urls": urls});
+            res.urls = urls;
+            next();
         }
     }     
     catch(err) {
-        //res.status(400).json({error: err});
+        res.status(400).json({error: err});
     }
-}
-
+})
