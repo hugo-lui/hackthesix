df8bed02fbf9c16ca2276d23069ecdee5f88a774
Hugo <hugo_lui@rogers.com>
2023-08-19 12:10:38 -0400

    add ticketmaster urls

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,36 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 // Util packages
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const [urls, setUrls] = useState([]);
+
+  const handleClick = async () => {
+    try {
+      const endpoint = `http://localhost:5000/${keyword}`;
+      const response = await fetch(endpoint);
+      if(response.ok) {
+        const jsonResponse = await response.json();
+        setUrls(jsonResponse.urls);
+      }
+    }     
+    catch(err) {
+        console.log(err);
+    }
+  }
+
+  const handleChange = (event) => {
+    setKeyword(event.target.value);
+  }
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home onChange={handleChange} onClick={handleClick} urls={urls}/>} />
       </Routes>
     </>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import HeroBanner from '../components/home/HeroBanner';
 
-const Home = () => {
+const Home = (props) => {
+  const urls = props.urls.map((url) => <li>{url}</li>);
+  
   return (
-    <div className="h-screen">
-      <HeroBanner/>
+    <div>
+      <div className="h-screen">
+        <HeroBanner/>
+      </div>
+      <div className="border-10"><input type="text" onChange={props.onChange}/></div>
+      <button onClick={props.onClick}>Submit</button>
+      <ul>{urls}</ul>
     </div>
+    
   );
 }
 
